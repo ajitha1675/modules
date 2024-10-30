2c1b781a91092beeada0fb2da96e17c49ec045a7
Ajilalitha <ajilalitha010@gmail.com>
2024-10-30 14:45:07 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import User from "./User";
+// import User from "./User";
 
-function App() {
-  const productData = {
-    product: "Mobile",
-    productprice: 23000,
-    productmodel: "moto g13",
-    productstorage: 4000,
-  };
+// function App() {
+//   const productData = {
+//     product: "Mobile",
+//     productprice: 23000,
+//     productmodel: "moto g13",
+//     productstorage: 4000,
+//   };
 
+
+//   return (
+//     <div className="App">
+//      <User 
+//      name={productData.product} 
+//      price={productData.productprice } 
+//      email={productData.productmodel}
+//      storage={productData.productstorage}
+//      />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React,{useState} from 'react'
+
+export const App = () => {
+
+ useState();
+ 
+ let x = 1;
+
+const handleAdd = () =>{
+  x = x + 1;
+  console.log("x",x);
+  
+}
 
   return (
-    <div className="App">
-     <User 
-     name={productData.product} 
-     price={productData.productprice } 
-     email={productData.productmodel}
-     storage={productData.productstorage}
-     />
-    </div>
-  );
+    <div>
+      <h1>{x}</h1>
+      <button onClick={handleAdd}>Add</button>
+      </div>
+  )
 }
 
 export default App;
