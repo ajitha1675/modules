7dc0df332de6c62cde73c6cafcb0589869c1142a
Ajilalitha <ajilalitha010@gmail.com>
2024-10-30 20:01:50 +0530

    useeffect

--- src/App.js
+++ src/App.js
@@ -23,33 +23,69 @@
 
 // export default App;
 
-import React,{useState} from 'react'
+// import React,{useState} from 'react'
 
-export const App = () => {
+// export const App = () => {
 
-const [num, setNum] =  useState(1);
+// const [num, setNum] =  useState(1);
 
- let x = 1;
+//  let x = 1;
 
-const handleAdd = () =>{
-  // x = x + 1;
-  // console.log("x",x);
-  // setNum(2);
-  setNum((currvalue) =>{
-    return currvalue + 1;
-  }
+// const handleAdd = () =>{
+//   // x = x + 1;
+//   // console.log("x",x);
+//   // setNum(2);
+//   setNum((currvalue) =>{
+//     return currvalue + 1;
+//   }
 
-)
+// )
   
-}
+// }
 
-// console.log(num);
+// // console.log(num);
+//   return (
+//     <div>
+//       <h1>{num}</h1>
+//       <button onClick={handleAdd}>Add</button>
+//       </div>
+//   )
+// }
+
+// export default App;
+
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+
+const App = () => {
+const [num, setnum] = useState(100)
+const [num2, setnum2] = useState(1000);
+useEffect(() =>{
+  setnum(200);
+  //setnum2(1000);
+  console.log("From useeffect");
+
+  return () =>{
+    setnum(100);
+    console.log("memory cleaned");
+    
+  };
+  
+}, [num, num2]);
+
+console.log("num", num);
+
+
   return (
     <div>
       <h1>{num}</h1>
-      <button onClick={handleAdd}>Add</button>
+      <button onClick={() => setnum((curr)=>curr + 1)}>
+      Add
+      </button>
+      <h1>{num2}</h1>
+      <button onClick={() => setnum2((curr)=> curr +1)}>Add</button>
       </div>
   )
-}
+};
 
 export default App;
